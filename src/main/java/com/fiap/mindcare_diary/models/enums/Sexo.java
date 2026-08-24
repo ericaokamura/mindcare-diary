a965f3dc8d22c8f8db20048db54ab194c7ce8cb8
erica.okamura@yahoo.com.br <erica.okamura@yahoo.com.br>
2026-08-24 01:48:21 -0300

    ajustes

--- src/main/java/com/fiap/mindcare_diary/models/enums/Sexo.java
+++ src/main/java/com/fiap/mindcare_diary/models/enums/Sexo.java
@@ -2,5 +2,13 @@ package com.fiap.mindcare_diary.models.enums;
 
 public enum Sexo {
 
-    FEMININO, MASCULINO;
+    FEMININO(0, "FEMININO"), MASCULINO(1, "MASCULINO");
+
+    public int codigo;
+    public String descricao;
+
+    Sexo(int codigo, String descricao) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+    }
 }
